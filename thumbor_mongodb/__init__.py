0225be45e2bd5ec5364e1f6c9d1d5764cd15afd6
Sergey pilyavskiy <pill.sv0@gmail.com>
2015-10-16 12:15:31 +0300

    change errors handling and add more configurability

--- thumbor_mongodb/__init__.py
+++ thumbor_mongodb/__init__.py
@@ -4,14 +4,17 @@
 # thumbor imaging service
 # https://github.com/globocom/thumbor/wiki
 
-# Licensed under the MIT license: 
+# Licensed under the MIT license:
 # http://www.opensource.org/licenses/mit-license
 
 # HBASE STORAGE OPTIONS
 from thumbor.config import Config
+
 Config.define('MONGO_LOADER_CNX_STRING', 'localhost', "MongodDB connexion string", 'mongo_loader')
 Config.define('MONGO_LOADER_SERVER_DB', 'thumbor', 'MongoDB database', 'mongo_loader')
 Config.define('MONGO_LOADER_SERVER_COLLECTION', 'images', 'MongoDB Collection', 'mongo_loader')
 Config.define('MONGO_LOADER_DOC_FIELD', 'content', 'MongoDB document field used for image content', 'mongo_loader')
+Config.define('MONGO_LOADER_ID_FIELD', '_id', 'MongoDB document field used for search', 'mongo_loader')
+Config.define('MONGO_LOADER_DATA_FMT', 'binary', 'MongoDB image format', 'mongo_loader')
 
 __version__ = "0.2"
--- thumbor_mongodb/loader.py
+++ thumbor_mongodb/loader.py
@@ -9,21 +9,51 @@
 # Copyright (c) 2011 figarocms dhardy@figarocms.fr
 
 import base64
+from bson.errors import InvalidId as InvalidIdError
 from pymongo import Connection
 from bson.objectid import ObjectId
 
-def __conn__(context):
-        connection = Connection(context.config.MONGO_LOADER_CNX_STRING)
-        db = connection[context.config.MONGO_LOADER_SERVER_DB]
-        storage = db[context.config.MONGO_LOADER_SERVER_COLLECTION]
 
-        return connection, db, storage
+def __conn__(context):
+    connection = Connection(context.config.MONGO_LOADER_CNX_STRING)
+    db = connection[context.config.MONGO_LOADER_SERVER_DB]
+    storage = db[context.config.MONGO_LOADER_SERVER_COLLECTION]
+    return connection, db, storage
+
 
 def load(context, url, callback):
     connection, db, storage = __conn__(context)
-    try:
-        document = storage.find_one({ '_id': ObjectId(url) }, { context.config.MONGO_LOADER_DOC_FIELD: True })
-        file_data = base64.b64decode(document[context.config.MONGO_LOADER_DOC_FIELD])
-        callback(file_data)
-    except (KeyError, TypeError):
+    search = url
+
+    if context.config.MONGO_LOADER_ID_FIELD == '_id':
+        try:
+            search = ObjectId(url)
+        except (TypeError, ValueError, InvalidIdError):
+            callback(None)
+            return
+
+    document = storage.find_one(
+        {context.config.MONGO_LOADER_ID_FIELD: search},
+        {context.config.MONGO_LOADER_DOC_FIELD: True}
+    )
+
+    if not document:
         callback(None)
+        return
+
+    try:
+        body = document[context.config.MONGO_LOADER_DOC_FIELD]
+    except KeyError:
+        callback(None)
+        return
+
+    if context.config.MONGO_LOADER_IMAGE_FMT == 'base64':
+        try:
+            file_data = base64.b64decode(body)
+        except TypeError:
+            callback(None)
+            return
+        else:
+            callback(file_data)
+    else:
+        callback(body)
